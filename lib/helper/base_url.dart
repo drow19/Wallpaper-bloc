23a739b28ad733edcc8b3cdb7cfd433d82758d73
drow19 <34456538+drow19@users.noreply.github.com>
2020-10-06 14:45:26 +0700

    delete

--- lib/helper/base_url.dart
+++
@@ -1,7 +0,0 @@
-String api_key = "";
-
-String baseUrl = "https://api.pexels.com/v1/curated?per_page=";
-
-String categoryUrl = "https://api.pexels.com/v1/search?query=";
-
-String detail = "https://api.pexels.com/v1/photos/";
--- lib/model/category_model.dart
+++
@@ -1,6 +0,0 @@
-class CategoryModel {
-  String id;
-  String img;
-
-  CategoryModel({this.id, this.img});
-}
